252c467299c459b27a5d2f172445ec814ce7e60a
David Dight <davidd@fix8mt.com>
2024-06-27 20:32:16 +1000

    pre-rel 1.0g

--- include/fix8/conjure_enum.hpp
+++ include/fix8/conjure_enum.hpp
@@ -83,25 +83,25 @@ public:
 };
 
 //-----------------------------------------------------------------------------------------
-class no_ctor_or_assign
+class no_ctor_assign_move
 {
 protected:
-	no_ctor_or_assign() = delete;
+	no_ctor_assign_move() = delete;
 #if defined _MSC_VER
-	~no_ctor_or_assign() = default; // warning C4624
+	~no_ctor_assign_move() = default; // warning C4624
 #else
-	~no_ctor_or_assign() = delete;
+	~no_ctor_assign_move() = delete;
 #endif
-	no_ctor_or_assign(const no_ctor_or_assign&) = delete;
-	no_ctor_or_assign& operator=(const no_ctor_or_assign&) = delete;
-	no_ctor_or_assign(no_ctor_or_assign&&) = delete;
-	no_ctor_or_assign& operator=(no_ctor_or_assign&&) = delete;
+	no_ctor_assign_move(const no_ctor_assign_move&) = delete;
+	no_ctor_assign_move& operator=(const no_ctor_assign_move&) = delete;
+	no_ctor_assign_move(no_ctor_assign_move&&) = delete;
+	no_ctor_assign_move& operator=(no_ctor_assign_move&&) = delete;
 };
 
 //-----------------------------------------------------------------------------------------
 // compiler specifics
 //-----------------------------------------------------------------------------------------
-class cs : public no_ctor_or_assign
+class cs : public no_ctor_assign_move
 {
 	static constexpr auto _specifics
 	{
@@ -151,7 +151,7 @@ concept valid_enum = requires(T)
 
 //-----------------------------------------------------------------------------------------
 template<valid_enum T>
-class conjure_enum : public no_ctor_or_assign
+class conjure_enum : public no_ctor_assign_move
 {
 	static constexpr int enum_min_value{ENUM_MIN_VALUE}, enum_max_value{ENUM_MAX_VALUE};
 	static_assert(enum_max_value > enum_min_value, "ENUM_MAX_VALUE must be greater than ENUM_MIN_VALUE");
@@ -768,7 +768,7 @@ constexpr enum_bitset<T> operator^(const enum_bitset<T>& lh, const enum_bitset<T
 //-----------------------------------------------------------------------------------------
 //-----------------------------------------------------------------------------------------
 template<typename T>
-class conjure_type : public no_ctor_or_assign
+class conjure_type : public no_ctor_assign_move
 {
 	static constexpr std::string_view _get_name() noexcept
 	{

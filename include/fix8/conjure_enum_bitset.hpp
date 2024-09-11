51c440e024c28fc8e5beae80aedb572d29f04d78
David Dight <dakka@fix8.org>
2024-09-11 16:40:17 +1000

    pre-rel 1.1.1a

--- include/fix8/conjure_enum_bitset.hpp
+++ include/fix8/conjure_enum_bitset.hpp
@@ -71,10 +71,11 @@ class enum_bitset
 				 std::conditional_t<countof <= 64, std::uint_least64_t, void>>>>;
 	static_assert(std::integral<U>, "requested bitset overflow");
 
-	static constexpr std::size_t bits_per_base = sizeof(U) * 8;
+	/*static constexpr std::size_t bits_per_base = sizeof(U) * 8;
 	static constexpr std::size_t base_type_count = (countof > 0 ? (countof - 1) / bits_per_base + 1 : 0);
 	static constexpr std::size_t not_interested = base_type_count * bits_per_base - countof;
-	static constexpr U all_bits = (U{1} << (bits_per_base - not_interested)) - 1;
+	static constexpr U all_bits = (std::size_t{1} << (bits_per_base - not_interested)) - 1; */
+	static constexpr U all_bits { (std::size_t{1} << countof) - 1 };
 	static constexpr U unused_bits { sizeof(U) * 8 - countof };
 
 	template <typename R>

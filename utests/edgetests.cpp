cccf1a984fca0f3c87b7a1f86d5b8f2ecac86ff3
David Dight <dakka@fix8.org>
2025-05-08 14:57:16 +1000

    msvc link error #1

--- utests/edgetests.cpp
+++ utests/edgetests.cpp
@@ -29,6 +29,7 @@
 // SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //----------------------------------------------------------------------------------------
 #include <catch2/catch_test_macros.hpp>
+#include <catch2/catch_all.hpp>
 #include <string_view>
 #include <fix8/conjure_enum.hpp>
 #include <fix8/conjure_enum_bitset.hpp>
